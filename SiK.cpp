effeb42c0c958b050ba98dd01926915d6d54acf5
Al3xDuda <117095097+Al3xDuda@users.noreply.github.com>
2022-12-05 19:04:41 +0100

    Add files via upload

--- SiK.cpp
+++ SiK.cpp
@@ -1,11 +1,16 @@
 #include <iostream>
 
+#include "Read_File.h"
 using namespace std;
 
 int main() {
 	char* test = new char[10];
 	cin >> test;
-	while(*test) {// ASCII check
-		printf("%d ", *test++);
-	}
+	Read_File nazwapliku = Read_File(test, 1);
+	string buffor = nazwapliku.Return_Buffor();
+	cout << endl;
+	cout << buffor << endl;
+
+	
+	
 }
