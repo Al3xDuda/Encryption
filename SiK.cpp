d8a13b69f1d734b00d0e20e6c36aafe0d0f48d6e
Al3xDuda <117095097+Al3xDuda@users.noreply.github.com>
2022-12-08 01:10:44 +0100

    Update SiK.cpp

--- SiK.cpp
+++ SiK.cpp
@@ -1,16 +1,28 @@
-#include <iostream>
-
-#include "Read_File.h"
+#include "RC4.h"
+#include "Manage_File.h"
+#define KEY "mention"
 using namespace std;
+//#define SHOW 
 
 int main() {
-	char* test = new char[10];
+	char* test = new char[256];
+	cout << "wpisz nazwe pliku ktory chcesz zaszyfrowac: ";
 	cin >> test;
-	Read_File nazwapliku = Read_File(test, 1);
+	File nazwapliku(test);
 	string buffor = nazwapliku.Return_Buffor();
+#ifdef SHOW
+	cout << nazwapliku.Return_File_Type() << endl;
 	cout << endl;
 	cout << buffor << endl;
-
-	
-	
+#endif 
+	RC4 var = RC4(buffor, KEY);
+#ifdef SHOW
+	var.print_S();
+	var.print_T();
+	for(int i = 0; i < nazwapliku.Return_Buffor().length() + 1; i++) {
+		printf("%c ", buffor[i]);
+	}
+#endif
+	return 0;
 }
+
