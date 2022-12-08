8fdb640b59ee1c123353749d0397701a82c0ea4b
Al3xDuda <117095097+Al3xDuda@users.noreply.github.com>
2022-12-08 01:09:02 +0100

    Update RC4.h

--- RC4.h
+++ RC4.h
@@ -1,1 +1,23 @@
 #pragma once
+
+#include <iostream>
+#include <string>
+#include <iomanip>
+#include "Manage_File.h"
+
+using namespace std;
+class RC4 {
+	int S[256];
+	int T[256];
+	size_t key_length;
+	string text_str;
+public:
+	RC4(string text, string key);
+	void Encrypt(string text, string* o_buff);
+	void Decrypt( string* o_buff);
+	void print_S();
+	void print_T();
+	void Init_T(string key);
+	void Swap_S();
+	void Init_S();
+};
