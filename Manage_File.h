2e582c79d0debacd609fd9012f389ff6d032bda1
Al3xDuda <117095097+Al3xDuda@users.noreply.github.com>
2022-12-23 21:52:43 +0100

    Update Manage_File.h

--- Manage_File.h
+++ Manage_File.h
@@ -2,17 +2,21 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <ctime>  
+
 using namespace std; 
 
 class File {
 public:
 	File();
 	File(char* sFileName);
+	
+	void Write(string sText, int x);
+	void Write(string sText);
+	
 	string Return_Buffor();
-	void Write(string sText,int x);
+	string console_file_read();
+	
 private:
 	string buffor;
 };
-
-
-
