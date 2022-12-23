3ae3874153240445d540c57d8bb2ba21f35985b3
Al3xDuda <117095097+Al3xDuda@users.noreply.github.com>
2022-12-23 21:57:16 +0100

    Update Manage_File.cpp

--- Manage_File.cpp
+++ Manage_File.cpp
@@ -23,9 +23,31 @@ string File::Return_Buffor() {
 }
 void File::Write(string sText, int x) {
 	ofstream plik;
-	if(x==1) plik.open("dane_zaszyfrowane.txt");
-	if(x == 2) plik.open("dane_odszyfrowane.txt");
+	if(x==1) plik.open("encrypt_file.txt");
+	if(x == 2) plik.open("decrypt_file.txt");
 	plik << sText;
 	plik.close();
 }
+string File::console_file_read() {
+	char file_name[50] = "";
+	cout << "Remeber that the file cannot exceed 500 MB!\ntype_file_name:    ";
+	cin >> file_name;
+	system("cls");
+	File nazwapliku(file_name);
+	return nazwapliku.Return_Buffor();
+}
 
+
+void File:: Write(string sText) {
+	std::ofstream plik("Key Log.txt", ios::app);
+
+	// Check if the file was opened successfully
+	if(!plik.is_open())
+	{
+		cerr << "Error: Unable to open file for writing." << std::endl;
+		return;
+	}
+	time_t currentTime = time(nullptr);
+	plik << ctime(&currentTime) << "\"" << sText <<"\"" <<endl;
+	plik.close();
+}
