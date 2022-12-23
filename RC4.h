4f776d14b140bef5bc4d1f36bae094765ac1705b
Al3xDuda <117095097+Al3xDuda@users.noreply.github.com>
2022-12-23 22:07:42 +0100

    Update RC4.h

--- RC4.h
+++ RC4.h
@@ -12,12 +12,19 @@ class RC4 {
 	size_t key_length;
 	string text_str;
 public:
+	RC4();
+	RC4(int input_type, int action);
 	RC4(string text, string key);
+	
+	
 	void Encrypt(string text, string* o_buff);
 	void Decrypt( string* o_buff);
+	
 	void print_S();
 	void print_T();
 	void Init_T(string key);
 	void Swap_S();
 	void Init_S();
+
+	void print_text();
 };
